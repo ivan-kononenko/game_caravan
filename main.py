6d9bea615a3c41f0476780b6f73d078488d1b0d1
ivan-kononenko <ivan.a.kononenko@gmail.com>
2023-09-25 13:03:53 +0300

    deck __str__ changes

--- main.py
+++ main.py
@@ -48,6 +48,14 @@ class Deck:
     def shuffle(self):
         return random.shuffle(self.cards)
 
+    def __str__(self):
+        #list amount of cards currently in deck
+        print(f"Deck currently has {len(self.cards)} cards.")
+
+    def deal(self, holder):
+        #receives list of players(p1, p2 and so on) and amount of card to give out to each player. Assign cards to player
+        print(f"{self.value} at {self.holder}")
+
 
 if __name__ == "__main__":
     print("Hi Caravan!")
@@ -55,3 +63,5 @@ if __name__ == "__main__":
     deck.shuffle()
     for card in deck.cards[:5]:
         print(card)
+
+    print(deck)
